18de670b1da242cb5f27cbdbf2ea5b697c8ec2f4
Razz67 <srasberry00@gmail.com>
2022-09-22 08:43:32 -0400

    Add comments

--- Pass-it-around/bottles.js
+++ Pass-it-around/bottles.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
+	// start with 99 bugs and add a link to the next page to change the number each time the link is clicked
 	res.send(
 		`99 bottles of beer on the wall, <a href="/98">Take one down and pass it around</a>`
 	);
@@ -11,18 +12,24 @@ app.get("/", function (req, res) {
 
 app.get("/:number_of_bottles", function (req, res) {
 	let number = Number(req.params.number_of_bottles);
+
+	// if number == 1 change bottles to bottle
 	if (number === 1) {
 		res.send(
 			`${number} bottle of beer on the wall, <a href="/${
 				number - 1
 			}">Take one down and pass it around</a>`
 		);
+
+		// if number is between 2 and 99 bottles = bottles
 	} else if (number > 1) {
 		res.send(
 			`${number} bottles of beer on the wall, <a href="/${
 				number - 1
 			}">Take one down and pass it around</a>`
 		);
+
+		// retun no more bottles
 	} else {
 		res.send("No more bottles of beer on the wall");
 	}
--- Pass-it-around/bugs-in-the-code.js
+++ Pass-it-around/bugs-in-the-code.js
@@ -4,32 +4,34 @@ const app = express();
 const port = 3000;
 
 app.get("/", function (req, res) {
-	res.send(
-		`99 little bugs in the code, <a href="/98">Take one down and patch it around</a>`
-	);
+	// start with 99 bugs and add a link to the next page to change the number each time the link is clicked
+	res.send(`99 little bugs in the code, <a href="/98">Take one down and patch it around</a>`);
 });
 
 app.get("/:number_of_bugs", function (req, res) {
 	let number = Number(req.params.number_of_bugs);
-    if(number % 5 === 0) {
-        number = Math.floor(Math.random(number) * 100);
-        res.send(
-            `${number} little bugs in the code, <a href="/${
-                number}">Take one down and patch it around</a>`
-        );
-    }  
-	else if (number === 1) {
+	// if number is divisible by 5 === 0 return a random number between 0 and 100
+	if (number % 5 === 0) {
+		number = Math.floor(Math.random(number) * 100);
+		res.send(
+			`${number} little bugs in the code, <a href="/${number}">Take one down and patch it around</a>`
+		);
+		// if number == 1 change bugs to bug
+	} else if (number === 1) {
 		res.send(
 			`${number} bug in the code, <a href="/${
-				number - 1}">Take one down and patch it around</a>`);
+				number - 1
+			}">Take one down and patch it around</a>`
+		);
+		// if number is between 2 and 99 bugs = bugs
 	} else if (number > 1) {
 		res.send(
 			`${number} little bugs in the code, <a href="/${
 				number - 1
 			}">Take one down and patch it around</a>`
 		);
-	}   
-    else {
+		// retun no more bugs
+	} else {
 		res.send("No more bugs in the code");
 	}
 });
