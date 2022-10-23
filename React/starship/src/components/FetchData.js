29c1ccf15a2a3561b7a1a45561745b223ee6e93f
Razz67 <srasberry00@gmail.com>
2022-10-23 09:55:18 -0400

    Add background image

---
+++ React/starship/src/components/FetchData.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { v4 } from "uuid";
+
+export default function App() {
+    
+	const [data, setData] = useState({ data: [] });
+	const [isLoading, setIsLoading] = useState(false);
+	const [err, setErr] = useState("");
+
+	const handleClick = async () => {
+		setIsLoading(true);
+
+		try {
+			const response = await fetch("https://swapi.dev/api/starships/", {
+				method: "GET",
+				headers: {
+					Accept: "application/json",
+				},
+			});
+
+			if (!response.ok) {
+				throw new Error(`Error! status: ${response.status}`);
+			}
+
+			const result = await response.json();
+
+			console.log("result is: ", JSON.stringify(result, null, 4));
+
+			setData(result);
+		} catch (err) {
+			setErr(err.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	console.log(data);
+
+	return (
+		<div>
+			{err && <h2>{err}</h2>}
+
+			<button onClick={handleClick}>Fetch data</button>
+
+			{isLoading && <h2>Loading...</h2>}
+
+			{data.data.map((ship) => {
+				return (
+					<div key={v4()}>
+						<h2>{ship.name}</h2>
+						<h2>{ship.model}</h2>
+						<h2>{ship.starship_class}</h2>
+						<br />
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
